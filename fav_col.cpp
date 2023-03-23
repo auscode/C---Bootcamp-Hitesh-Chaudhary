c121675d4765966a233093bae6ab1a20d54349d8
Harshit Mishra <76650831+auscode@users.noreply.github.com>
2023-03-23 19:56:11 +0530

    favcol

--- fav_col.cpp
+++ fav_col.cpp
@@ -4,12 +4,17 @@
 
 int main()
 {
-    // std::cout << "Hello There" << std::endl;
-    // puts("Hello There");
-
-    int number;
-    std::cin >> number;
-    printf("Your id is %d", number + 3);
-    //%d is placeholder its vale calculated after comma
+    std::string my_color;
+    std::cout << "Enter your fav. color: \n";
+    getline(std::cin, my_color); // for taking input instead of cin
+                                 //  getline(which method to use , where you want to store )
+    std::cout << "Hey " << my_color << " is my favorite too"<<std::endl;
     return 0;
 }
+// std::cout << "Hello There" << std::endl;
+// puts("Hello There");
+
+// int number;
+// std::cin >> number;
+// printf("Your id is %d", number + 3);
+//%d is placeholder its vale calculated after comma
